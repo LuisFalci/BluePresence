48a00e77b89a345291f96252ce4f13c12382cbd4
Luis Gustavo Furtuozo Falci <luisgustavo.1999@hotmail.com>
2023-08-30 15:00:33 -0300

    listagem dos dias

--- app/src/main/java/com/example/a16_room/ui/view/subject/CreateSubjectActivity.kt
+++ app/src/main/java/com/example/a16_room/ui/view/subject/CreateSubjectActivity.kt
@@ -1,26 +1,32 @@
 package com.example.a16_room.ui.view.subject
 
-import android.app.TimePickerDialog
 import android.os.Bundle
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.Spinner
+import android.widget.CheckBox
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.a16_room.R
 import com.example.a16_room.databinding.ActivityCreateSubjectBinding
 import com.example.a16_room.ui.viewmodels.SubjectViewModel
-import java.util.Calendar
 
 class CreateSubjectActivity : AppCompatActivity() {
     private lateinit var viewModel: SubjectViewModel
     lateinit var binding: ActivityCreateSubjectBinding
-    private lateinit var mainLayout: LinearLayout
-    private lateinit var showOptionsButton: Button
-    private lateinit var optionsSpinner: Spinner
     private val selectedOptions = mutableListOf<String>()
+    private lateinit var dayListView: ListView
+    private lateinit var selectedDaysArrayAdapter: ArrayAdapter<String>
+
+
+    private val checkBoxes: List<CheckBox> by lazy {
+        listOf(
+                binding.fragmentCreatealarmCheckMon,
+                binding.fragmentCreatealarmCheckTue,
+                binding.fragmentCreatealarmCheckWed,
+                binding.fragmentCreatealarmCheckThu,
+                binding.fragmentCreatealarmCheckFri
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,73 +41,22 @@ class CreateSubjectActivity : AppCompatActivity() {
             viewModel.insert(name)
             finish()
         }
+        val options = listOf("Segunda", "Terça", "Quarta", "Quinta", "Sexta")
 
-        mainLayout = findViewById(R.id.main_layout)
-        optionsSpinner = findViewById(R.id.options_spinner)
-
-        val options = listOf("","Segunda", "Terça", "Quarta", "Quinta", "Sexta")
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
-        optionsSpinner.adapter = adapter
-
-        var isFirstItemSelected = true
-        optionsSpinner.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: android.view.View?, position: Int, id: Long) {
-                if (!isFirstItemSelected) {
-                    val selectedOption = options[position]
-                    if(selectedOptions.contains(selectedOption)){
-                        return
-                    }
-                    selectedOptions.add(selectedOption)
+        checkBoxes.forEachIndexed { index, checkBox ->
+            checkBox.setOnCheckedChangeListener { _, isChecked ->
+                val day = options[index]
+                if (isChecked) {
+                    selectedOptions.add(day)
                 } else {
-                    isFirstItemSelected = false // Altera para false após a primeira seleção
+                    selectedOptions.remove(day)
                 }
+                dayListView = findViewById(R.id.dayListView)
+                selectedDaysArrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, selectedOptions)
+                dayListView.adapter = selectedDaysArrayAdapter
             }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                selectedOptions.clear()
-            }
-        })
-
-        binding.buttonPickStartTime.setOnClickListener {
-            showTimePickerDialog(true)
         }
 
-        binding.buttonPickEndTime.setOnClickListener {
-            showTimePickerDialog(false)
-        }
 
-        binding.buttonInsertDay.setOnClickListener {
-            val selectedOptionsText = selectedOptions.joinToString(", ")
-            binding.textSelectedOptions .text = "Opções selecionadas: $selectedOptionsText"
-            val startTimeText = binding.buttonPickStartTime.text.toString()
-            val endTimeText = binding.buttonPickEndTime.text.toString()
-
-            val selectedText = "Opções selecionadas: $selectedOptionsText\n" +
-                    "Horário: $startTimeText até $endTimeText"
-
-            binding.textSelectedOptionsAndTimes.text = selectedText
-        }
-    }
-    private fun showTimePickerDialog(isStartTime: Boolean) {
-        val currentTime = Calendar.getInstance()
-        val hour = currentTime.get(Calendar.HOUR_OF_DAY)
-        val minute = currentTime.get(Calendar.MINUTE)
-
-        val timePickerDialog = TimePickerDialog(
-                this,
-                { _, selectedHour, selectedMinute ->
-                    val selectedTime = String.format("%02d:%02d", selectedHour, selectedMinute)
-                    if (isStartTime) {
-                        binding.buttonPickStartTime.setText(selectedTime)
-                    } else {
-                        binding.buttonPickEndTime.setText(selectedTime)
-                    }
-                },
-                hour,
-                minute,
-                true
-        )
-
-        timePickerDialog.show()
     }
 }
