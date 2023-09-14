8c112b79d35393b37059a40c4c24d9e95705ca58
Luis Gustavo Furtuozo Falci <luisgustavo.1999@hotmail.com>
2023-09-14 16:27:59 -0300

    tabela attendance

--- app/src/main/java/com/example/a16_room/data/repository/AttendanceRepository.kt
+++ app/src/main/java/com/example/a16_room/data/repository/AttendanceRepository.kt
@@ -10,4 +10,22 @@ class AttendanceRepository(context: Context) {
     fun insert(attendanceModel: AttendanceModel){
         return attendanceDAO.insert(attendanceModel)
     }
+    fun update(attendance: AttendanceModel): Int {
+        return attendanceDAO.update(attendance)
+    }
+
+    fun delete(attendance: AttendanceModel): Int {
+        return attendanceDAO.delete(attendance)
+    }
+
+    fun get(attendanceId: Long): AttendanceModel {
+        return attendanceDAO.get(attendanceId)
+    }
+
+    fun getAll(): List<AttendanceModel> {
+        return attendanceDAO.getAll()
+    }
+    fun getAllAttendancesForStudent(studentId: Long): List<AttendanceModel> {
+        return attendanceDAO.getAllAttendancesForStudent(studentId)
+    }
 }
--- app/src/main/java/com/example/a16_room/ui/viewmodels/AttendanceViewModel.kt
+++ app/src/main/java/com/example/a16_room/ui/viewmodels/AttendanceViewModel.kt
@@ -2,11 +2,18 @@ package com.example.a16_room.ui.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import com.example.a16_room.data.models.AttendanceModel
 import com.example.a16_room.data.repository.AttendanceRepository
 
 class AttendanceViewModel(application: Application) : AndroidViewModel(application) {
     private val repository = AttendanceRepository(application.applicationContext)
+
+    private val listAttendances = MutableLiveData<List<AttendanceModel>>()
+
+    private val attendance = MutableLiveData<AttendanceModel>()
+
+    private var changes = MutableLiveData<Long>()
 
     fun insert(studentId: Long, presence: Boolean, dateTime: String) {
         val model = AttendanceModel().apply {
@@ -14,5 +21,43 @@ class AttendanceViewModel(application: Application) : AndroidViewModel(applicati
             this.presence = presence
         }
         repository.insert(model)
+    }
+
+    fun getAll() {
+        listAttendances.value = repository.getAll()
+    }
+
+    fun get(studentId: Long) {
+        attendance.value = repository.get(studentId)
+    }
+
+    fun update(
+        attendanceId: Long,
+        studentId: Long,
+        dateTime: String,
+        presence: Boolean
+    ): Int {
+        val model = AttendanceModel().apply {
+            this.attendanceId = attendanceId
+            this.student_id = studentId
+            this.dateTime = dateTime
+            this.presence = presence
+        }
+        val affectedRows = repository.update(model)
+        changes.value = affectedRows.toLong()
+        return affectedRows
+    }
+
+    fun delete(attendanceId: Long): Int {
+        val model = AttendanceModel().apply {
+            this.attendanceId = attendanceId
+        }
+        val affectedRows = repository.delete(model)
+        changes.value = affectedRows.toLong()
+        return affectedRows
+    }
+
+    fun getAllAttendancesForStudent(studentId: Long) {
+        listAttendances.value = repository.getAllAttendancesForStudent(studentId)
     }
 }
